7da3a040ea49e72eafba9a4fac06a60ee6931bdf
Marty Fitchen and Sky Rousse <sky@>
2016-05-31 15:38:54 -0700

    add ul nav for selecting coversion type and scripts to reveal and hide

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // begin Gallons to Liters calc
+/*
 var gallon = parseInt(prompt("How many gallons are used:"));
 
 var liters = function(gallon) {
@@ -8,4 +9,26 @@ var liters = function(gallon) {
 var convertedLiters = liters(gallon);
 
 alert(gallon + " gallons is equal to " + convertedLiters + " liters.");
+*/
 // end Gallons to Liters calc
+
+//begin jQuery
+
+$("#gallons-show").click(function() {
+  $("#gallons-show").toggle();
+  $("#gallons-hidden").toggle();
+  $(".show-button").toggle();
+});
+
+$("#Tsp-show").click(function() {
+  $("#Tsp-show").toggle();
+  $("#Tsp-hidden").toggle();
+});
+/*$("#gallons-hidden").click(function() {
+  $(".closed").show();
+});
+
+$("#Tsp-hidden").click(function() {
+  $(".closed").show();
+});
+*/
