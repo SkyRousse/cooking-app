133c625a6f5cd6a4d3c7f7ebcaa2c62c3067717f
George Olson and Sky Rousse <george@>
2016-06-01 15:18:29 -0700

    add business logic and user interface logic to scripts.js file. Add bootstrap files and link to them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
-// begin Gallons to Liters calc
-/*
-var gallon = parseInt(prompt("How many gallons are used:"));
-
-var liters = function(gallon) {
-	return gallon / .26417;
+//business logic
+var multiply = function(number1, number2) {
+	return number1 * number2;
 };
 
-var convertedLiters = liters(gallon);
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
 
-alert(gallon + " gallons is equal to " + convertedLiters + " liters.");
-*/
+// user interface logic
+$(document).ready(function() {
+  $("form#gallons-liters").submit(function(event) {
+    event.preventDefault();
+    var conversionNumber = .26417;
+    var gallon = parseInt($("input#gallons").val());
+    var calculatedLiters = divide(gallon, conversionNumber);
+    $("#results").text(calculatedLiters);
+  });
+});
+
+$(document).ready(function() {
+  $("form#tsp-floz").submit(function(event){
+    event.preventDefault();
+    var conversionNumber = 0.16667;
+    var tsp = $("input#tsp").val();
+    var calculatedFloz = multiply(tsp, conversionNumber);
+    $("#results").text(calculatedFloz);
+  });
+});
+
 // end Gallons to Liters calc
 
 //begin jQuery
-
+/*
 $("#gallons-show").click(function() {
   $("#gallons-show").toggle();
   $("#gallons-hidden").toggle();
@@ -24,6 +42,7 @@ $("#Tsp-show").click(function() {
   $("#Tsp-show").toggle();
   $("#Tsp-hidden").toggle();
 });
+*/
 /*$("#gallons-hidden").click(function() {
   $(".closed").show();
 });
